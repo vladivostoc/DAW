1d0ef3ed0c126ecfbb338c56a0d9a271008c96a4
jose vladimir nuñez arevalo <jose.vladimir.nunez@gmail>
2014-02-20 20:34:08 -0600

    Pk2_RSVPwapp
    ultima practica

---
+++ Pk2_RSVPwapp/Pk2_RSVPwapp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pk2_RSVPwapp.Models;// se  importara el espacio de nombre 
+
+namespace Pk2_RSVPwapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ?
+                "Buenos Dias!!!" : "Buena Tarde!!!";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //verificando errores de validacion
+            if (ModelState.IsValid)
+            {
+                //enviar respuesta del organizador del evento
+                return View("agradecimientoss", guestResponse);
+            }
+            else {
+                //si hay un error de validacion 
+                return View();
+            }
+            
+        }
+    }
+}
